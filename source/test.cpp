44b41bc0c1e57c75b87931ad29d7d24db2f14890
Travace Reese <tsuk13@gmail.com>
2012-10-19 21:34:49 -0400

    made test write the whole size of screen

--- source/test.cpp
+++ source/test.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 
 int main(){
-  //int vertLines = 24;
-  //int horzLines = 80;
-  for(int i = 0; i < 80; i++){
-    std::cout << "#";
+  int vertLines = 24;
+  int horzLines = 80;
+  for(int i = 0; i < vertLines; i++){
+    for(int j = 0; j < horzLines; j++){
+      std::cout << "#";
+    }
+    std::cout<<"\n";
   }
-  std::cout <<"\n";
   return 0;
 }
